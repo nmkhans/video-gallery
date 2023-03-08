c79ec1200688188e99122c740855f5fef806e327
nmkhans <nurmoin05@gmail.com>
2023-03-09 00:21:58 +0600

    video item rendered

--- src/components/VideoGallery/VideoGallery.jsx
+++ src/components/VideoGallery/VideoGallery.jsx
@@ -5,22 +5,22 @@ import { fetchVideos } from "../../redux/reducers/videoSlice/videoSlice";
 
 const VideoGallery = () => {
   const dispatch = useDispatch();
-  const videos = useSelector((state) => state.videos.videos);
+  const videos = useSelector((state) => state.videos);
 
   useEffect(() => {
     dispatch(fetchVideos());
   }, []);
 
+  if(videos?.isLoading) return "Loading..."
+
   return (
     <section className="pt-12">
       <section className="pt-12">
         <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
-          {videos.map((video) => (
-            <VideoItem />
+          {videos?.videos?.map((video) => (
+            <VideoItem key={video.id} video={video} />
           ))}
-
-          {/* <!-- error section
-                    <div className="col-span-12">some error happened</div> --> */}
+          <div className="col-span-12">{videos.isError && videos.error}</div>
         </div>
       </section>
     </section>
--- src/components/VideoItam/VideoItem.jsx
+++ src/components/VideoItam/VideoItem.jsx
@@ -1,47 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const VideoItem = () => {
+const VideoItem = ({ video }) => {
+  const { id, title, author, avatar, date, duration, thumbnail, views } = video;
+
   return (
     <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
       <div className="w-full flex flex-col">
         <div className="relative">
-          <Link to="/video/1">
+          <Link to={`/video/${id}`}>
             <img
-              src="https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg"
+              src={thumbnail}
               className="w-full h-auto"
               alt="Some video title"
             />
           </Link>
 
           <p className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
-            12:10
+            {duration}
           </p>
         </div>
 
         <div className="flex flex-row mt-2 gap-2">
-          <Link to="/video/1" className="shrink-0">
+          <Link to={`/video/${id}`} className="shrink-0">
             <img
-              src="https://avatars.githubusercontent.com/u/73503432?v=4"
+              src={avatar}
               className="rounded-full h-6 w-6"
               alt="Learn with Sumit"
             />
           </Link>
 
           <div clas="flex flex-col">
-            <Link to="/video/1">
-              <p className="text-slate-900 text-sm font-semibold">
-                Video title
-              </p>
+            <Link to={`/video/${id}`}>
+              <p className="text-slate-900 text-sm font-semibold">{title}</p>
             </Link>
             <Link
               className="text-gray-400 text-xs mt-2 hover:text-gray-600"
-              to="/video/1"
+              to={`/video/${id}`}
             >
-              Learn with Sumit
+              {author}
             </Link>
             <p className="text-gray-400 text-xs mt-1">
-              200 views . May 3, 2022
+              {views} views . {date}
             </p>
           </div>
         </div>
